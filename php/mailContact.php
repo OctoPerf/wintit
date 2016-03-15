f133712584393a372b25fcfab979cc45425d7278
edeltil <edeltil@gmail.com>
2016-03-15 22:55:40 +0100

    send mail

---
+++ php/mailContact.php
@@ -0,0 +1,36 @@
+<?php
+if($_POST){
+    	//$message = $_POST['data'];
+
+$message = '<html><body>';
+$message .= '<table cellpadding="0" cellspacing="0" style="width: 100%;"><tbody>';
+$message .= '<tr><td>Civilite</td><td>'.$_POST['civilite'].'</td></tr>';
+$message .= '<tr><td>Nom, prenom</td><td>'.$_POST['name'].'</td></tr>';
+$message .= '<tr><td>Fonction</td><td>'.$_POST['ffunction'].'</td></tr>';
+$message .= '<tr><td>Societe</td><td>'.$_POST['company'].'</td></tr>';
+$message .= '<tr><td>Adresse de courriel</td><td>'.$_POST['email'].'</td></tr>';
+$message .= '<tr><td>Numero de telephone</td><td>'.$_POST['phone'].'</td></tr>';
+$message .= '<tr><td>Comment avez vous connu notre site ?&nbsp;</td><td>'.$_POST['connaissance'].'</td></tr>';
+$message .= '<tr><td>Vos questions et/ou suggestions</td><td>'.$_POST['message'].'</td></tr>';
+$message .= '</tbody></table></body></html>';
+
+	$copy = $_POST['copy'];
+	//send email
+	$to      = 'edeltil@gmail.com';//'bettina.gerecht@wintit.com, rene.khlat@wintit.com, bbruet@wintit.com'
+	//$Expediteur = 'postmaster@wintit.com';
+	$subject = "Message d'un visiteur sur le site WINTIT";
+	
+	$headers = 'From: edeltil@gmail.com' . "\r\n" .
+    		'Reply-To: edeltil@gmail.com' . "\r\n" .
+    		'X-Mailer: PHP/' . phpversion();
+
+   	if(trim($DestinatairesCaches)) $headers .= 'Bcc: '.$DestinatairesCaches."\r\n";
+   	if($copy===FALSE) $headers .= 'From: '.$Expediteur."\r\n";
+
+	$headers .= 'MIME-Version: 1.0'."\r\n";
+   	$headers .= 'Content-Type: text/html; charset=iso-8859-1'."\r\n";
+    	$headers .= 'X-Mailer: PHP/'.phpversion()."\r\n";
+
+	mail($to, $subject, $message, $headers);
+}
+?>
